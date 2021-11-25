12460dbc65d9353b048ed7fe55c6261fde65b9ae
jfgrissom <jfgrissom@gmail.com>
2021-11-25 09:33:31 -0800

    Initial commit.

---
+++ test/flare-node-aws-static-resources.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as FlareNodeAwsStaticResources from '../lib/flare-node-aws-static-resources-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/flare-node-aws-static-resources-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FlareNodeAwsStaticResources.FlareNodeAwsStaticResourcesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
